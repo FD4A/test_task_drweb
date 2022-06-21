eb6db7551ee176d26ea695cde63fac224fda2253
dimon <End-ik@ya.ru>
2022-06-22 00:26:43 +0300

    more tests
    +tests with color struct

--- MAIN_drweb_dictionary.cpp
+++ MAIN_drweb_dictionary.cpp
@@ -3,10 +3,14 @@
 
 void test_simple();
 void test_exception();
+void test_simple_string_and_struct();
+void test_exception_string_and_struct();
 
 int main(int argc, char **argv)
 {
     test_simple();
     test_exception();
+    test_simple_string_and_struct();
+    test_exception_string_and_struct();
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -33,7 +33,7 @@ void test_simple()
     assert( xblue==colorB );
 
     std::cout << D;
-    std::cout << "[OK] finish " << __FUNCTION__ << "()\n";
+    std::cout << "[OK] finish " << __FUNCTION__ << "()\n" << std::endl;
 }
 
 void test_exception()
@@ -49,7 +49,8 @@ void test_exception()
     catch ( const not_found_exception_mydict<std::string>& msg )
     {
     	std::cerr << "not_found_exception_mydict " << msg.get_key() << '\n';
-    	std::cout << "[OK] finish " << __FUNCTION__ << "()\n";
+    	std::flush(std::cerr);
+    	std::cout << "[OK] finish " << __FUNCTION__ << "()\n" << std::endl;
     	return;
     }
     bool resul_test_exception = false;
@@ -57,5 +58,81 @@ void test_exception()
 }
 
 
+struct color
+{
+	unsigned char r=255;
+	unsigned char g=255;
+	unsigned char b=255;
+	color() = default;
+	color(unsigned char r_, unsigned char g_, unsigned char b_): r{r_}, g{g_}, b{b_}
+		{}
 
+	bool operator==(const color& c) const
+	{
+		if(r==c.r && g==c.g && b==c.b)
+			{return true;}
+		return false;
+	}
 
+	friend std::ostream& operator<<(std::ostream& os, const color& c)
+	{
+		os << static_cast<unsigned int>(c.r) << ' ' << static_cast<unsigned int>(c.g) << ' ' << static_cast<unsigned int>(c.b);
+		return os;
+	}
+};
+void test_simple_string_and_struct()
+{
+    std::cout << "start " << __FUNCTION__ << '\n';
+
+    concrete_dictionary<std::string,color> D;
+
+    std::string red("RED");
+    color sred(255,0,0);
+    std::string green("GREEN");
+    color sgreen(0,255,0);
+    std::string blue("BLUE");
+    color sblue(0,0,255);
+    std::string white("WHITE");
+
+    D.set(red, sred);
+    D.set(green, sgreen);
+    D.set(blue, sblue);
+
+    assert( true==D.is_set(red) );
+    assert( true==D.is_set(green) );
+    assert( true==D.is_set(blue) );
+    assert( false==D.is_set(white) );
+
+    color colorR = D.get(red);
+    color colorG = D.get(green);
+    color colorB = D.get(blue);
+
+    assert( sred==colorR );
+    assert( sgreen==colorG );
+    assert( sblue==colorB );
+
+    std::cout << D;
+    std::cout << "[OK] finish " << __FUNCTION__ << "()\n\n";
+}
+
+void test_exception_string_and_struct()
+{
+    std::cout << "start " << __FUNCTION__ << '\n';
+    concrete_dictionary<std::string,color> D;
+    std::string white("WHITE");
+
+    try
+    {
+    	color colorW = D.get(white);
+    }
+    catch ( const not_found_exception_mydict<std::string>& msg )
+    {
+    	std::flush(std::cout);
+    	std::cerr << "not_found_exception_mydict " << msg.get_key() << '\n';
+    	std::flush(std::cerr);
+    	std::cout << "[OK] finish " << __FUNCTION__ << "()\n" << std::endl;
+    	return;
+    }
+    bool test_exception_string_and_struct = false;
+    assert(test_exception_string_and_struct);
+}

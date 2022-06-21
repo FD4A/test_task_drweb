3927fca7c6d2ad62d68c5e8f18db8fe60c4617c0
FDA <40368675+FD4A@users.noreply.github.com>
2022-06-21 15:29:40 +0300

    fix tabs
    tabs problem

--- concrete_dictionary.hpp
+++ concrete_dictionary.hpp
@@ -14,9 +14,9 @@ public:
 	not_found_exception_mydict(Key key): key_{key}
 		{}
 
-    const Key& get_key() const noexcept override
+    	const Key& get_key() const noexcept override
 	{
-    	return key_;
+    		return key_;
 	}
 };
 
@@ -26,32 +26,30 @@ class concrete_dictionary: public dictionary<Key,Value>
 private:
 	std::unordered_map<Key,Value> storage;
 public:
-    ~concrete_dictionary(){};
+    	~concrete_dictionary(){};
 
-    const Value& get(const Key& key) const override
-    {
-    	auto item = storage.find(key);
-    	if(nullptr==item)
+    	const Value& get(const Key& key) const override
     	{
-    		throw not_found_exception_mydict<Key>(key);
+    		auto item = storage.find(key);
+    		if(nullptr==item)
+    			{throw not_found_exception_mydict<Key>(key);}
+    		return (*item).second;
     	}
-    	return (*item).second;
-    }
 
-    void set(const Key& key, const Value& value) override
-    {
-    	 const auto [it, success] = storage.insert( std::pair<Key,Value>(key,value) );
-    	 if(!success)
-    	 	 {std::cerr << "fail set pair[" << key << "," << value << "]\n";}
-    }
+    	void set(const Key& key, const Value& value) override
+    	{
+    		const auto [it, success] = storage.insert( std::pair<Key,Value>(key,value) );
+    	 	if(!success)
+    	 		{std::cerr << "fail set pair[" << key << "," << value << "]\n";}
+    	}
 
-    bool is_set(const Key& key) const override
-    {
-    	auto item = storage.find(key);
-    	if(nullptr==item)
-    		{return false;}
-    	return true;
-    }
+    	bool is_set(const Key& key) const override
+    	{
+    		auto item = storage.find(key);
+    		if(nullptr==item)
+    			{return false;}
+    		return true;
+    	}
 
 	friend std::ostream& operator<<(std::ostream& os, const concrete_dictionary<Key,Value>& dict)
 	{
